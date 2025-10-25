ad883814fdde82cafa337777a3fdda346b09db46
zorgonide <ammar1998@gmail.com>
2025-10-25 22:52:17 +0100

    refactor MoviePoster component for improved layout and styling of image and release date

--- src/components/ui/MoviePoster.tsx
+++ src/components/ui/MoviePoster.tsx
@@ -5,17 +5,21 @@ import { tmdbMovie } from "@/lib/types";
 export default function MoviePoster({ movie }: { movie: tmdbMovie }) {
   return (
     <Link href={`/movie/${movie.id}`} key={movie.id}>
-      <div className="flex h-full cursor-pointer flex-col overflow-hidden rounded-sm bg-mxpink transition-transform hover:scale-105 hover:bg-mxpink-hover">
-        <Image
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt={movie.title}
-          className="h-auto w-full"
-          width={500}
-          height={750}
-        />
-        <div className="flex p-2">
+      <div className="flex h-full cursor-pointer flex-col overflow-hidden rounded-sm bg-slate-800 transition-transform hover:scale-105">
+        <div className="relative w-full aspect-[2/3]">
+          <Image
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+            className="object-cover"
+            fill
+            sizes="(max-width: 768px) 50vw, (max-width: 1200px) 25vw, 17vw"
+          />
+        </div>
+        <div className="flex flex-grow flex-col p-2">
           <h3 className="truncate text-sm font-semibold">{movie.title}</h3>
-          <h3 className="ml-auto text-sm">{movie.release_date?.slice(0, 4)}</h3>
+          <h3 className="mt-auto text-xs text-gray-400">
+            {movie.release_date?.slice(0, 4)}
+          </h3>
         </div>
       </div>
     </Link>
