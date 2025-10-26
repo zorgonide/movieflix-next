eec9e679b98f76534d2dda0c9686db337410c891
zorgonide <ammar1998@gmail.com>
2025-10-26 21:11:18 +0000

    fix: update MoviePoster component to handle missing poster paths and improve type definition

--- src/components/ui/movie/MoviePoster.tsx
+++ src/components/ui/movie/MoviePoster.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
 import CustomLink from "@/components/ui/CustomLink"; // Use the new CustomLink
-import { tmdbMovie } from "@/lib/types";
-
-export default function MoviePoster({ movie }: { movie: tmdbMovie }) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function MoviePoster({ movie }: { movie: any }) {
   return (
     <CustomLink href={`/movie/${movie.id}`} key={movie.id}>
       <div className="flex h-full cursor-pointer flex-col overflow-hidden rounded-sm bg-slate-800 transition-transform hover:scale-101">
         <div className="relative w-full aspect-[2/3]">
           <Image
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            src={`https://image.tmdb.org/t/p/w500${
+              movie.poster_path || movie.poster
+            }`}
             alt={movie.title}
             className="object-cover"
             fill
