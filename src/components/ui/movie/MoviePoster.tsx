b64ccd86c8d24df32982d1e9a6f46963eb867095
zorgonide <ammar1998@gmail.com>
2025-10-26 11:00:32 +0000

    fix: adjust hover scale effect in MoviePoster component for improved user experience

--- src/components/ui/movie/MoviePoster.tsx
+++ src/components/ui/movie/MoviePoster.tsx
@@ -5,7 +5,7 @@ import { tmdbMovie } from "@/lib/types";
 export default function MoviePoster({ movie }: { movie: tmdbMovie }) {
   return (
     <CustomLink href={`/movie/${movie.id}`} key={movie.id}>
-      <div className="flex h-full cursor-pointer flex-col overflow-hidden rounded-sm bg-slate-800 transition-transform hover:scale-105">
+      <div className="flex h-full cursor-pointer flex-col overflow-hidden rounded-sm bg-slate-800 transition-transform hover:scale-101">
         <div className="relative w-full aspect-[2/3]">
           <Image
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
